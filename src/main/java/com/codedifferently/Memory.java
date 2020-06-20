05b3ad92cc1ca8a36e76c85dbbc72580c8437dc1
Kaveesha <weerasirikaveesha@gmail.com>
2020-06-20 10:19:27 -0500

    added tests and class for memory

--- src/main/java/com/codedifferently/Memory.java
+++ src/main/java/com/codedifferently/Memory.java
@@ -1,5 +1,17 @@
 package com.codedifferently;
 
 public class Memory {
-    
+    private double valueInMemory;
+
+    public void addMemory(double amount) {
+        this.valueInMemory = amount;
+    }
+
+    public void resetMemory() {
+        this.valueInMemory = 0;
+    }
+
+    public double recallValue() {
+        return this.valueInMemory;
+    }
 }
--- src/test/java/com/codedifferently/MemoryTest.java
+++ src/test/java/com/codedifferently/MemoryTest.java
@@ -1,5 +1,51 @@
 package com.codedifferently;
 
+import org.junit.*;
+
 public class MemoryTest {
-    
+    @Test
+    public void addMemoryTest() {
+         //given
+         double expectedResult = 153.4;
+
+         //when
+         Memory mem = new Memory();
+ 
+         mem.addMemory(153.4);
+         double actualResult = mem.recallValue();
+ 
+         //then
+         Assert.assertEquals(expectedResult, actualResult, 0.01);
+    }
+
+    @Test
+    public void recallValueTest() {
+         //given
+         double expectedResult = 190.54;
+
+         //when
+         Memory mem = new Memory();
+ 
+         mem.addMemory(190.54);
+         double actualResult = mem.recallValue();
+ 
+         //then
+         Assert.assertEquals(expectedResult, actualResult, 0.01);
+    }
+
+    @Test
+    public void resetMemoryTest() {
+         //given
+         double expectedResult = 0.0;
+
+         //when
+         Memory mem = new Memory();
+ 
+         mem.addMemory(190.54);
+         mem.resetMemory();
+         double actualResult = mem.recallValue();
+ 
+         //then
+         Assert.assertEquals(expectedResult, actualResult, 0.01);
+    }
 }
