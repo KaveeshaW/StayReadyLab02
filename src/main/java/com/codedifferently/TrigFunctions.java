d646c96a2af8176d3760fbc1055a9393acd6b17e
Kaveesha <weerasirikaveesha@gmail.com>
2020-06-20 15:35:54 -0500

    trig function stuff

--- src/main/java/com/codedifferently/TrigFunctions.java
+++ src/main/java/com/codedifferently/TrigFunctions.java
@@ -1,7 +1,26 @@
 package com.codedifferently;
 
-public class TrigFunctions { 
-    public double sine() {
-        return 0.0;
+public class TrigFunctions {
+    public double sine(){
+        double degrees = 45.0;
+        double radians = Math.to(degrees);
+        double sinValue = Math.sin(radians);
+        return sinValue;
+    }
+    public double cos(){
+        double degrees = 45.0;
+        double radians = Math.toRadians(degrees);
+        double cosValue = Math.cos(radians);
+        return cosValue;
+    }
+    public double tan(){
+        double degrees = 45.0;
+        double radians = Math.toRadians(degrees);
+        double tanValue = Math.tan(degrees);
+        return tanValue;
+    }
+    public double invCos(){
+        double degrees = 0.0;
+        return degrees;
     }
 }
