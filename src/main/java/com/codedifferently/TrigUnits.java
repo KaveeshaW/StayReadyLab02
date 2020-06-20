1ab7970448f29aa75d33ec319e9c84f3f92c9386
Kaveesha <weerasirikaveesha@gmail.com>
2020-06-20 15:06:42 -0500

    TrigUnits Update

--- src/main/java/com/codedifferently/TrigUnits.java
+++ src/main/java/com/codedifferently/TrigUnits.java
@@ -1,9 +1,23 @@
 package com.codedifferently;
 
+//degrees and radians
 public class TrigUnits {
     private String whichUnit;
 
+    public TrigUnits() {
+        this.whichUnit = "degrees";
+    }
+
     public String getMode() {
         return this.whichUnit;
+    }
+
+    //NEED TO DO
+    public void switchUnitsMode() {
+
+    }
+
+    public void switchUnitsMode(String mode) {
+        this.whichUnit = mode;
     }
 }
--- src/test/java/com/codedifferently/TrigUnitsTest.java
+++ src/test/java/com/codedifferently/TrigUnitsTest.java
@@ -3,12 +3,60 @@ package com.codedifferently;
 import org.junit.*;
 
 public class TrigUnitsTest {
+
+    @Test
+    public void constructorTest() {
+        //given
+        String expectedValue = "degrees";
+
+        //when
+        TrigUnits units = new TrigUnits();
+        String actualValue = units.getMode();
+
+        //then
+        Assert.assertEquals(expectedValue, actualValue);
+    }
+
     @Test
     public void getModeTest() {
         //given
-
-        //when
+        String expectedValue = "degrees";
         
+        //when
+        TrigUnits units = new TrigUnits();
+        String actualValue = units.getMode();
+
         //then
+        Assert.assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void switchUnitsWithArgsTest() {
+        //given
+        String expectedValue = "radians";
+        
+        //when
+        TrigUnits units = new TrigUnits();
+        units.switchUnitsMode("radians");
+        String actualValue = units.getMode();
+
+        //then
+        Assert.assertEquals(expectedValue, actualValue);
+    }
+
+
+    //NEED TO FIX
+    @Test
+    public void switchUnitsNoArgsTest() {
+        //given
+        String expectedValue = "radians";
+        
+        //when
+        TrigUnits units = new TrigUnits();
+        units.switchUnitsMode("radians");
+        String actualValue = units.getMode();
+
+        //then
+        Assert.assertEquals(expectedValue, actualValue);
     }
 }
