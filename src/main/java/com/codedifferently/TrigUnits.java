4450128cb8ecfbc2e53b1160cb8ec099606fd809
Kaveesha <weerasirikaveesha@gmail.com>
2020-06-20 14:30:50 -0500

    updated UML

---
+++ src/main/java/com/codedifferently/TrigUnits.java
@@ -0,0 +1,9 @@
+package com.codedifferently;
+
+public class TrigUnits {
+    private String whichUnit;
+
+    public String getMode() {
+        return this.whichUnit;
+    }
+}
---
+++ src/test/java/com/codedifferently/TrigUnitsTest.java
@@ -0,0 +1,14 @@
+package com.codedifferently;
+
+import org.junit.*;
+
+public class TrigUnitsTest {
+    @Test
+    public void getModeTest() {
+        //given
+
+        //when
+        
+        //then
+    }
+}
