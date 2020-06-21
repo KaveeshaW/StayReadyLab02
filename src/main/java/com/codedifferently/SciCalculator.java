14a8e6daa3099611322d8e5ad3c17599c0941986
Kaveesha <weerasirikaveesha@gmail.com>
2020-06-21 18:52:43 -0500

    code that you'll like

--- src/main/java/com/codedifferently/SciCalculator.java
+++ src/main/java/com/codedifferently/SciCalculator.java
@@ -1,5 +1,5 @@
 package com.codedifferently;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Hello world!
@@ -14,6 +14,7 @@ public class SciCalculator
     private TrigUnits trigUnits;
     private TrigFunctions trigFunctions;
     private Memory memory;
+    private HashMap <String, Integer> map;
 
     //constructor
     public SciCalculator() {
@@ -22,6 +23,7 @@ public class SciCalculator
         this.displayMode = new DisplayMode();
         this.trigUnits = new TrigUnits();
         this.trigFunctions = new TrigFunctions();
+        this.map = new HashMap<String, Integer>();
     }
 
     //list function names
@@ -29,6 +31,7 @@ public class SciCalculator
     public static void main( String[] args )
     {
         SciCalculator calc = new SciCalculator();
+        calc.instantiateMap(calc.map);
         System.out.println(calc.sayHello());
         System.out.println(calc.getDisplayValue());
 
@@ -44,99 +47,86 @@ public class SciCalculator
 
             //first gets the method name, then the inputted numbers
             String method = userChoice.next();
+            //saves typing toLowerCase for every if statement
+            method = method.toLowerCase();
             
-            if(method.toLowerCase().equals("quit")) {
+            if(method.equals("quit")) {
                 System.out.println("Have a nice day!!!");
                 break;
             }
             //trig goes right into asking about degrees or radians, so we want to skip this part of the loop
-            if(!method.toLowerCase().equals("trig")) {
-                System.out.println("How many numbers are you going to input?");
-                numInput = userChoice.nextDouble();
-                //only print out this statement if greater than 0
-                //do not want user to be prompted again if its a method that does not take in any parameters
-                if(numInput > 0 && numInput <= 4) {
-                    System.out.println("Enter the inputs one at a time, then press enter"); 
-                }
-                if(numInput > 4) {
-                    System.out.println("A number greater than 4 is not allowed because the maximum number of inputs you can have is 4. Please try again.");
+            if(!method.equals("trig")) {
+                //the map does not have the method, reprompt the user
+                if(calc.map.containsKey(method) == false) {
+                    System.out.println("That method cannot be found. Please try again.");
                     System.out.println("Please enter a command name. To quit, type 'quit'.");
                     continue;
                 }
-                if(numInput < 0) {
-                    System.out.println("Cannot enter a negative number. Please try again.");
-                    System.out.println("Please enter a command name. To quit, type 'quit'.");
-                    continue;
+                //tracks how many inputs one has to enter for each method to work
+                int numInputs = calc.map.get(method);
+                //if the number of inputs is zero, there is nothing to print out here, so go call the function
+                if(numInputs != 0) {
+                    System.out.println("Enter " + numInputs + " input(s) one at a time, then press enter"); 
                 }
-                
-                for(int i = 0; i < numInput; i++) {
+                for(int i = 0; i < numInputs; i++) {
                     userNumArr[i] = userChoice.nextDouble();
                 }
-
-                //TESTING PURPOSES
-                // for(int i = 0; i < numInput; i++) {
-                //     System.out.println("inputted numbers: " + userNumArr[i]);
-                // }
                 
                 if(method.toLowerCase().equals("quad")) {
                     System.out.println("Do you want the positive or negative value? Type positive (can also just type 'pos') or negative (can also just type 'neg').");
                     System.out.println("Note that typing positive does not gaurentee getting a positive number, it just gaurentees it gives you the answer with the + sign on the top of the fraction.");
                     posOrNeg = userChoice.next();
                 }
-            }
-            //TESTING PURPOSES
-            //System.out.println("userInput: " + userNumber);
-            //System.out.println("method: " + method);
-            
+            }            
 
             //2. call functions with if statements
-            if(method.toLowerCase().equals("add")) {
+            if(method.equals("add")) {
                 double addition = calc.alu.add(userNumArr[0], calc.displayValue);
                 calc.displayResult(calc, addition, "add");
             }
-            else if(method.toLowerCase().equals("subtract")) {
+            else if(method.equals("subtract")) {
                 double subtraction = calc.alu.subtract(userNumArr[0], calc.displayValue);
                 calc.displayResult(calc, subtraction, "subtract");
             }
-            else if(method.toLowerCase().equals("multiply")) {
+            else if(method.equals("multiply")) {
                 double multiply = calc.alu.multiply(userNumArr[0], calc.displayValue);
                 calc.displayResult(calc, multiply, "multiply");
             }
-            else if(method.toLowerCase().equals("divide")) {
+            else if(method.equals("divide")) {
                 double divide = calc.alu.divide(userNumArr[0], calc.displayValue);
                 calc.displayResult(calc, divide, "divide");
             }
-            else if(method.toLowerCase().equals("square")) {
+            else if(method.equals("square")) {
                 double square = calc.alu.calculateSquare(calc.displayValue);
                 calc.displayResult(calc, square, "calculateSquare");
             }  
-            else if(method.toLowerCase().equals("squareroot")) {
+            else if(method.equals("squareroot")) {
                 double squareRoot = calc.alu.calculateSquareRoot(calc.displayValue);
                 calc.displayResult(calc, squareRoot, "calculateSquareRoot");
             } 
-            else if(method.toLowerCase().equals("exp")) {
+            else if(method.equals("exp")) {
                 double exp = calc.alu.calculateExponential(userNumArr[0], calc.displayValue);
                 calc.displayResult(calc, exp, "calculateExponential");
             } 
-            else if(method.toLowerCase().equals("inverse")) {
+            else if(method.equals("inverse")) {
                 double inverse = calc.alu.calculateInverse(calc.displayValue);
                 calc.displayResult(calc, inverse, "calculateInverse");
             } 
-            else if(method.toLowerCase().equals("invert")) {
+            else if(method.equals("invert")) {
                 double invertedNum = calc.alu.invertSign(calc.displayValue);
                 calc.displayResult(calc, invertedNum, "invertSign");
             } 
-            else if(method.toLowerCase().equals("distance")) {
+            else if(method.equals("distance")) {
                 calc.valueNotUsedWarning("computeDistanceFormula");
                 double distance = calc.alu.computeDistanceFormula(userNumArr[0], userNumArr[1], userNumArr[2], userNumArr[3], calc.displayValue);
                 calc.displayResult(calc, distance, "computeDistanceFormula");
             }
-            else if(method.toLowerCase().equals("quad")) {
+            else if(method.equals("quad")) {
                 calc.valueNotUsedWarning("computeQuadraticFormula");
                 double quadResult = calc.alu.computeQuadraticFormula(userNumArr[0], userNumArr[1], userNumArr[2], posOrNeg, calc.displayValue);
                 calc.displayResult(calc, quadResult, "computeQuadraticFormula");
             }
-            else if(method.toLowerCase().equals("hyp")) {
+            else if(method.equals("hyp")) {
                 calc.valueNotUsedWarning("computeHypotenus");
                 double hyp = calc.alu.computeHypotenus(userNumArr[0], userNumArr[1]);
                 calc.displayResult(calc, hyp, "computeHypotenus");
@@ -152,6 +142,8 @@ public class SciCalculator
                     
                     //takes in whether it is degrees or radians, .next() takes in the next string
                     String userTrigMode = userChoice.next(); 
+                    //saves typing toLowerCase for every if statement
+                    userTrigMode = userTrigMode.toLowerCase();
 
                     //is it degrees or radians??
                     boolean trigMode = TrigUnits.toTrigModeForUser(userTrigMode);
@@ -159,17 +151,35 @@ public class SciCalculator
                     System.out.println("What trig method do you want to use?");
                     //gets the method name i.e cos, sin, tan, etc.
                     String trigMethod = userChoice.next();
+                    //saves typing toLowerCase for every if statement
+                    trigMethod = trigMethod.toLowerCase();
                     
-                    if(method.equals("quit")) {
+                    if(trigMethod.equals("quit")) {
                         System.out.println("Have a nice day!!!");
                         break;
                     }
                     
-                    System.out.println("How many numbers are you going to input?");
-                    numInput = userChoice.nextDouble();
-                    System.out.println("Enter the inputs one at a time, then press enter");
+                    // System.out.println("How many numbers are you going to input?");
+                    // numInput = userChoice.nextDouble();
+                    // System.out.println("Enter the inputs one at a time, then press enter");
                     
-                    for(int i = 0; i < numInput; i++) {
+                    // for(int i = 0; i < numInput; i++) {
+                    //     userNumArr[i] = userChoice.nextDouble();
+                    // }
+
+                    //the map does not have the method, reprompt the user
+                    if(calc.map.containsKey(trigMethod) == false) {
+                        System.out.println("That method cannot be found. Please try again.");
+                        System.out.println("Please enter a command name. To quit, type 'quit'.");
+                        continue;
+                    }
+                    //tracks how many inputs one has to enter for each method to work
+                    int numInputs = calc.map.get(trigMethod);
+                    //if the number of inputs is zero, there is nothing to print out here, so go call the function
+                    if(numInputs != 0) {
+                        System.out.println("Enter " + numInputs + " input(s) one at a time, then press enter"); 
+                    }
+                    for(int i = 0; i < numInputs; i++) {
                         userNumArr[i] = userChoice.nextDouble();
                     }
 
@@ -243,6 +253,23 @@ public class SciCalculator
     }
     public Memory getMemory() {
         return this.memory;
+    }
+
+    //maps to each function how many inputs is needed to make life simpler for the user when using the UI
+    public void instantiateMap(HashMap <String, Integer> map) {
+        this.map.put("add", 1);;
+        this.map.put("subtract", 1);
+        this.map.put("multiply", 1);
+        this.map.put("divide", 1);
+        this.map.put("square", 0);
+        this.map.put("squareroot", 0);
+        this.map.put("exp", 1);
+        this.map.put("inverse", 0);
+        this.map.put("invert", 0);
+        this.map.put("distance", 4);
+        this.map.put("quad", 3);
+        this.map.put("hyp", 2);
+        this.map.put("sine", 1);
     }
 }
  
